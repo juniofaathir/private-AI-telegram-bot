cce36036963124fcca4be75c73fc2c473b7183d4
junio <junio@jkt1.ebdesk.com>
2024-11-17 00:09:39 +0700

    refactor; add window memory size params

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,39 +7,42 @@ from ai_chat import AIChat
 class TelegramAIBot:
     """
     Telegram bot integrated with Local LLM to provide AI responses. Supports multiple chat sessions for multiple users.
+    
+    Parameters:
+    ----------
+    telegram_token (str): Token bot Telegram
+    base_url (str): Local LLM URL with OpenAI schema
+    openai_api_key (str): API key OpenAI
+    system_prompt (str, optional): AI's system prompt
+    model (str): Model name
+    memory_size (int): Chat memory window
     """
     def __init__(
         self,
         telegram_token: str,
         base_url: str,
         openai_api_key: str,
+        model: str,
         system_prompt: Optional[str] = None,
-        model: str = "gpt-3.5-turbo"
+        memory_size: int = 20
     ):
-        """
-        Args:
-            telegram_token (str): Token bot Telegram
-            openai_api_key (str): API key OpenAI
-            system_prompt (str, optional): System prompt untuk AI
-            model (str, optional): Model OpenAI yang digunakan
-        """
         self.telegram_token = telegram_token
         self.base_url = base_url
         self.openai_api_key = openai_api_key
         self.system_prompt = system_prompt
         self.model = model
+        self.memory_size = memory_size
         # Save chat instance for each user
         self.chat_sessions: Dict[int, AIChat] = {}
         
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Handler for command /start"""
-        user_id = update.effective_user.id
         user_name = update.effective_user.first_name
         
         welcome_message = (
             f"Halo {user_name}! 👋\n\n"
-            "Saya Nina, asisten digital AI buatan Lord Jofand.\n"
-            "Jadi ada yang bisa saya bantu hari ini?.\n\n"
+            "Aku Nina, asisten digital AI."
+            "Jadi ada yang bisa kubantu hari ini?.\n\n"
             "Beberapa command yang tersedia:\n"
             "/start - Memulai bot\n"
             "/clear - Menghapus riwayat chat\n"
@@ -90,6 +93,7 @@ class TelegramAIBot:
                 base_url = self.base_url,
                 system_prompt = self.system_prompt,
                 model = self.model,
+                memory_size = self.memory_size,
                 timezone = "Asia/Jakarta"
             )
         return self.chat_sessions[user_id]
